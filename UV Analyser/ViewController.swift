5d2efcb31e7b53514a2166bd2bf03c899e042308
Tim Snow <tim.snow@cunninglemon.com>
2015-05-21 13:03:14 +0100

    Initial Commit

---
+++ UV Analyser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UV Analyser
+//
+//  Created by Tim Snow on 21/05/2015.
+//  Copyright (c) 2015 Tim Snow. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
